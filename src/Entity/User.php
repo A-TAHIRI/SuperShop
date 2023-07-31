3868b3b8dd0e4822cdab6d86a5ef23be2a3eba73
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-07-31 23:59:46 +0200

    ...

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,148 +1,40 @@
 <?php
 
-namespace App\Entity ;
+namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * User
- *
- * @ORM\Table(name="user", uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_8D93D649E7927C74", columns={"email"})})
- * @ORM\Entity
- */
-class User
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=180, nullable=false)
+     * @var string The hashed password
      */
-    private $email;
+    #[ORM\Column]
+    private ?string $password = null;
 
-    /**
-     * @var array
-     *
-     * @ORM\Column(name="roles", type="json", nullable=false)
-     */
-    private $roles;
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255, nullable=false)
-     */
-    private $password;
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="surname", type="string", length=255, nullable=false)
-     */
-    private $surname;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255, nullable=false)
-     */
-    private $name;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="mob_tel", type="string", length=255, nullable=false)
-     */
-    private $mobTel;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="city", type="string", length=255, nullable=false)
-     */
-    private $city;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="code_zip", type="string", length=10, nullable=false)
-     */
-    private $codeZip;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="street", type="string", length=255, nullable=false)
-     */
-    private $street;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="build_num", type="string", length=10, nullable=false)
-     */
-    private $buildNum;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="avatar_image", type="string", length=255, nullable=true)
-     */
-    private $avatarImage;
-
-    /**
-     * @var bool
-     *
-     * @ORM\Column(name="is_verified", type="boolean", nullable=false)
-     */
-    private $isVerified;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="reset_token", type="string", length=255, nullable=true)
-     */
-    private $resetToken;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="additional_info", type="text", length=0, nullable=true)
-     */
-    private $additionalInfo;
-
-    /**
-     * @var datetime_immutable
-     *
-     * @ORM\Column(name="created_at", type="datetime_immutable", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
-     */
-    private $createdAt = 'CURRENT_TIMESTAMP';
-
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     *
-     * @ORM\ManyToMany(targetEntity="Product", mappedBy="users")
-     */
-    private $products = array();
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
 
     public function getId(): ?int
     {
@@ -161,9 +53,26 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
@@ -173,7 +82,10 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -185,14 +97,23 @@ class User
         return $this;
     }
 
-    public function getSurname(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
     {
-        return $this->surname;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
-    public function setSurname(string $surname): static
+    public function getCreatedAt(): ?\DateTimeImmutable
     {
-        $this->surname = $surname;
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
 
         return $this;
     }
@@ -209,151 +130,15 @@ class User
         return $this;
     }
 
-    public function getMobTel(): ?string
+    public function getImage(): ?string
     {
-        return $this->mobTel;
+        return $this->image;
     }
 
-    public function setMobTel(string $mobTel): static
+    public function setImage(?string $image): static
     {
-        $this->mobTel = $mobTel;
+        $this->image = $image;
 
         return $this;
     }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): static
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getCodeZip(): ?string
-    {
-        return $this->codeZip;
-    }
-
-    public function setCodeZip(string $codeZip): static
-    {
-        $this->codeZip = $codeZip;
-
-        return $this;
-    }
-
-    public function getStreet(): ?string
-    {
-        return $this->street;
-    }
-
-    public function setStreet(string $street): static
-    {
-        $this->street = $street;
-
-        return $this;
-    }
-
-    public function getBuildNum(): ?string
-    {
-        return $this->buildNum;
-    }
-
-    public function setBuildNum(string $buildNum): static
-    {
-        $this->buildNum = $buildNum;
-
-        return $this;
-    }
-
-    public function getAvatarImage(): ?string
-    {
-        return $this->avatarImage;
-    }
-
-    public function setAvatarImage(?string $avatarImage): static
-    {
-        $this->avatarImage = $avatarImage;
-
-        return $this;
-    }
-
-    public function isIsVerified(): ?bool
-    {
-        return $this->isVerified;
-    }
-
-    public function setIsVerified(bool $isVerified): static
-    {
-        $this->isVerified = $isVerified;
-
-        return $this;
-    }
-
-    public function getResetToken(): ?string
-    {
-        return $this->resetToken;
-    }
-
-    public function setResetToken(?string $resetToken): static
-    {
-        $this->resetToken = $resetToken;
-
-        return $this;
-    }
-
-    public function getAdditionalInfo(): ?string
-    {
-        return $this->additionalInfo;
-    }
-
-    public function setAdditionalInfo(?string $additionalInfo): static
-    {
-        $this->additionalInfo = $additionalInfo;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Product>
-     */
-    public function getProducts(): Collection
-    {
-        return $this->products;
-    }
-
-    public function addProduct(Product $product): static
-    {
-        if (!$this->products->contains($product)) {
-            $this->products->add($product);
-            $product->addUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProduct(Product $product): static
-    {
-        if ($this->products->removeElement($product)) {
-            $product->removeUser($this);
-        }
-
-        return $this;
-    }
-
 }
